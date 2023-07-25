c0e22a7dfe6a36d00cb471ad1e5a3e84322a9eb7
Joel K George <joelkgeorge@Joels-MBP.local>
2023-07-25 20:50:12 +0530

    String

---
+++ String Manipulation/String.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char str1[50], str2[50],str3[50];
+    int i = 0, j=0,k=0;
+    printf("Enter The First String: ");
+    gets(str1);
+    printf("Enter the second String: ");
+    gets(str2);
+    while(str1[i]!='\0'){
+        str3[]
+    }
+}
